ae54270b58961c04c7ac3907b1b1036e8fc01a20
MoiseenkoInit <139032589+MoiseenkoInit@users.noreply.github.com>
2023-07-27 19:41:47 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+Console.Clear();
+Console.WriteLine("введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+int grade = a;
+
+for (int i = 1; i < b; i++)
+{
+grade = grade * a;
+}
+Console.WriteLine("a в степени b равно: " + grade);
