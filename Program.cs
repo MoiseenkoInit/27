5f2b868e89002c9e739695559cb80f5ab2d6c9b0
MoiseenkoInit <139032589+MoiseenkoInit@users.noreply.github.com>
2023-08-05 11:57:03 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,17 +1,17 @@
-﻿//Задача 27: Напишите программу, которая принимает 
-//на вход число и выдаёт сумму цифр в числе.
-//452 -> 11
-//82 -> 10
-//9012 -> 12
+﻿// Задача 43. Напишите программу, которая найдёт точку 
+//пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1 y = k2 * x + b2 значения b1, k1, b2 и k2 задаются пользователем.
 
-Console.Write("Input N: ");
-int N = Convert.ToInt32(Console.ReadLine());
-int sum = 0;
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
 
-while (N > 0)
-{
-    int k = N % 10;
-    N = N / 10;
-    sum = sum + k;
-}
-Console.Write("sum of fingers " + sum);
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
